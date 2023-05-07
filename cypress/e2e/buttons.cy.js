2a1f5cde2734b3ddc463e423fb43f43cfa79c3a0
vburduja11 <vburduja11@gmail.com>
2023-05-07 10:21:56 -0500

    buttons

---
+++ cypress/e2e/buttons.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Context: My First Tests', () => {
+
+
+    beforeEach(() => {
+        // run before each test case, beforeMethod in TestNG
+        cy.clearCookies();
+        cy.visit('multiple_buttons')
+    })
+    it('check different buttons actions', () =>{
+        //select a button with text
+
+        cy.contains('Button 2').should('be.visible').click();
+        cy.contains('Clicked on button two!').should('be.visible');
+    })
+
+})
