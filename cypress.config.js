3e0a1b5120863d2594f6b8eac9ae82799172adb2
vburduja11 <vburduja11@gmail.com>
2023-05-07 11:28:16 -0500

    Done for Sunday Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://practice.cydeo.com/',
     video: false,
-    retries: 2,
-    defaultCommandTimeout: 8,
+    retries: 1,
+    defaultCommandTimeout: 5000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -13,42 +13,46 @@ describe('Find or Get Elements by Using Different Locators', () => {
        // attribute name and value
        cy.get("[type='text']").clear();  // clear what is typed
 
+       // tagName
        cy.get("input").each((item, index, list) => {
         // assert the length of the list is 2
         expect(list).to.have.length(2);
         expect(item).to.have.attr("type");
        })
-       //by atribute
-    cy.get('[type]');
+       
+       // by attribute name
+       cy.get('[type]');
 
-    //by class name
-    cy.get('.btn.btn-primary');
+       // by className
+       cy.get('.btn.btn-primary');
 
-    //by ID
-    cy.get('#wooden_spoon');
+       // By id
+       cy.get('#wooden_spoon');
 
-    // if I want to use text: no xpath in cypress, but it still possible with a different approach
-    cy.get('button').should('contain','Login').click();
+       // if I want to use text: no xpath in cypress, but it still possible with a different approach
+       cy.get('button').should('contain','Login').click();
+    
     })
-    it('Check finding elements by traveling throuth DOM',() => {
-       // travel to find the login button: locate username box- go to parent form -then find button
+
+    it('Check finding elements by traveling through DOM', () => {
+        // travel to find the login button: locate username box- go to parent form -then find button
         cy.get('input[name="username"]').parents('form').find('button').should('contain','Login').click();
+
     })
 
-    it('Check Different type of Assertions', () =>{
-        // cypress itself usees assertions provieded by Chai, Sinon and jQuery libraries
-        //Shoud Assertion: does the assertion directly on the objects itself
+    it.only('Check Different Type of Assertions', () => {
+        // Cypress itself bundles assertions provided by Chai, Sinon and jQuery libraries
+        // Should Assertion: does the assertion directly on the object itself
         cy.get('#wooden_spoon')
         .should('contain','Login')
         .and('have.class','btn btn-primary');
-
-        //expect Assertion: explicit, creates a subject of our test, the u implement different actions
-        cy.get('#wooden_spoon')
-        .then((buttonElement)=> {
+        // expect assertion: creates a subject of our test, then you implement different actions 
+        cy.get('#wooden_spoon').then((buttonElement) => {
             expect(buttonElement).to.have.text('Login');
-            expect(buttonElement).to.have.class('btn btn-primary')
+            expect(buttonElement).to.have.class('btn btn-primary');
         })
-
     })
+
+
     
 })
